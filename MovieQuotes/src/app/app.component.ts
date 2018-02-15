557e90459a11286d3b2a900a7b548da691fe8cd9
Ben Blood <bluealikai@gmail.com>
2018-02-14 20:44:54 -0500

    ng lint improvements

--- MovieQuotes/src/app/app.component.ts
+++ MovieQuotes/src/app/app.component.ts
@@ -16,26 +16,17 @@ interface MovieQuote {
 export class AppComponent {
   private itemDoc: AngularFirestoreDocument<MovieQuote>;
   private itemsCollection: AngularFirestoreCollection<MovieQuote>;
-  readonly quotesPath = "quotes";
-  readonly collectionPath = "collection"
+  readonly quotesPath = 'quotes';
+  readonly collectionPath = 'collection';
 
   formMovieQuote: MovieQuote = {
     'quote': '',
     'movie': ''
   };
 
-  // Local only solution
-  // movieQuotes: MovieQuote[] = [
-  //   {"movie": "Rocky", "quote": "Yo Adrian"},
-  //   {"movie": "Terminator", "quote": "I'll be back"},
-  //   {"movie": "Titanic", "quote": "I'm the king of the world!"},
-  //   {"movie": "The Princess Bride", "quote": "Hello. My name is Inigo Montoya. You killed my father. Prepare to die."}
-  // ];
-
   movieQuotesStream: Observable<MovieQuote[]>;
   constructor(db: AngularFirestore) {
-    this.itemsCollection  = db.collection<MovieQuote>(this.quotesPath);
-    
+    this.itemsCollection = db.collection<MovieQuote>(this.quotesPath);
     this.movieQuotesStream = this.itemsCollection.snapshotChanges().map(actions => {
       return actions.map(a => {
         const data = a.payload.doc.data() as MovieQuote;
@@ -43,13 +34,9 @@ export class AppComponent {
         return { $key, ...data };
       });
     });
-    //this.movieQuotesStream = this.itemsCollection.valueChanges();
-
   }
 
   onSubmit(): void {
-    // Local only solution
-    // this.movieQuotes.unshift(this.formMovieQuote);
     try {
       if (this.formMovieQuote.$key) {
         this.itemsCollection.doc(this.formMovieQuote.$key).update(this.formMovieQuote);
@@ -60,10 +47,7 @@ export class AppComponent {
         'quote': '',
         'movie': ''
       };
-    }
-    catch (e) {
-      console.log("Form error:", e);
-    }
+    } catch (e) { console.log('Form error:', e); }
   }
 
   edit(movieQuote: MovieQuote): void {
--- MovieQuotes/src/environments/environment.prod.ts
+++ MovieQuotes/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyBQBb-MJo_p4-gT9S6GuWq3VSDlhh5XWNo",
-    authDomain: "bblood-movie-quotes.firebaseapp.com",
-    databaseURL: "https://bblood-movie-quotes.firebaseio.com",
-    projectId: "bblood-movie-quotes",
-    storageBucket: "bblood-movie-quotes.appspot.com",
-    messagingSenderId: "399575498158"
+    apiKey: 'AIzaSyBQBb-MJo_p4-gT9S6GuWq3VSDlhh5XWNo',
+    authDomain: 'bblood-movie-quotes.firebaseapp.com',
+    databaseURL: 'https://bblood-movie-quotes.firebaseio.com',
+    projectId: 'bblood-movie-quotes',
+    storageBucket: 'bblood-movie-quotes.appspot.com',
+    messagingSenderId: '399575498158'
   },
 };
